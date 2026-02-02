3c47ec0a0823339916cc2c6a395a38fdb5eaae0b
royonem <roywmyung@gmail.com>
2026-02-02 11:14:57 +0900

    First commit

--- src/main/java/web/repository/UserRepository.java
+++ src/main/java/web/repository/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
-import javax.persistence.Table;
+import jakarta.persistence.Table;
 
 @Repository
 @Table(name = "users")
